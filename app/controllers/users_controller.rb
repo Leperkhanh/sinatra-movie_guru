ac7c2cda237b1fff0ac30764a48fb54c9628b3f7
Jordan Nguyen <jordan@DESKTOP-GP7UG4A.localdomain>
2020-08-01 13:04:11 -0400

    added users controllers and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,67 @@ class UsersController < ApplicationController
 
   # GET: /users
   get "/users" do
+    @users = User.all
     erb :"/users/index.html"
   end
 
+  get "/users/login" do
+    if !logged_in? 
+      erb :"/users/login.html"
+    else
+      flash[:message] = "You are already logged in"  
+      redirect "/users/show"  
+    end  
+  end
+
+  post "/users/login" do
+    user = User.find_by(:username => params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      flash[:message] = "Welcome back, #{user.username}"
+      redirect "/users/show"
+    else
+      flash[:error] = "Login failed. Please try again or create a new account"
+      redirect "/users/login"
+    end
+end
+
+
   # GET: /users/new
   get "/users/new" do
-    erb :"/users/new.html"
+    if !logged_in?
+      erb :"/users/new.html"
+    else
+      redirect "/users/show" 
+    end   
+  end
+
+  get "/users/logout" do
+    if logged_in?
+      session.destroy
+      flash[:message] = "You have successfully logged out"
+      redirect to 'users/login'
+    else
+      redirect to '/'
+    end
   end
 
   # POST: /users
   post "/users" do
-    redirect "/users"
+    @user = User.create(params[:user])
+    flash[:message] = "Account created! Please Log In"
+    redirect "/users/login"
   end
 
   # GET: /users/5
   get "/users/:id" do
-    erb :"/users/show.html"
+    if logged_in?
+      @user = current_user
+      @reviews = @user.reviews
+      erb :"/users/show.html"
+    else  
+      redirect "/users/login"
+    end    
   end
 
   # GET: /users/5/edit
